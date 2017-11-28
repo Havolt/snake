a12b4f9fbc1aee9e68d619976af6b78085d75b0f
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-28 17:44:12 +0000

    Start button added to canvas

--- snakeScript.js
+++ snakeScript.js
@@ -1,18 +1,36 @@
 
 let snakeCan;
+let ctx;
+const canSize = 480;
 
 function createCanvas(){
   const c = document.createElement('canvas');
   c.id = "snakeCan";
   app.appendChild(c);
+  snakeCan = document.getElementById('snakeCan');
+  ctx = snakeCan.getContext('2d');
+  ctx.canvas.width = canSize;
+  ctx.canvas.height = canSize;
+}
+
+function createMenu(){
+  ctx.fillStyle="#141619";
+  ctx.fillRect(0,0,canSize,canSize);
+  const bu = document.createElement('button');
+  bu.id="startButton";
+  bu.innerHTML="Start";
+  app.appendChild(bu);
 }
 
 function assigner(){
-  snakeCan = document.getElementById('snakeCan');
+
 }
+
+
 
 
 (function init(){
   createCanvas();
+  createMenu();
   assigner();
 })();
