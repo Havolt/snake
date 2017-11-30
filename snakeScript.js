aac86a4f1868a3875e954eeeebfc997b9684c771
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-30 20:44:07 +0000

    Snake body now follows head

--- snakeScript.js
+++ snakeScript.js
@@ -4,7 +4,7 @@ let ctx;
 const tileSize = 20;
 const canSize = 480;
 const tileAmt = 12;
-let gameSpeed = 400;
+let gameSpeed = 300;
 let snake = {};
 
 function createCanvas(){
@@ -70,16 +70,27 @@ function snakeDraw(){
   ctx.fillRect(0,0,canSize,canSize);
   ctx.fillStyle="white";
   ctx.fillRect((snake.headX*tileSize),(snake.headY*tileSize),tileSize,tileSize);
+  for(var i = 1; i < snake.body.length;i++){
+    ctx.fillStyle="white";
+    ctx.fillRect((snake.body[i].x*tileSize),(snake.body[i].y*tileSize),tileSize,tileSize);
+  }
 }
 
 function snakeTail(){
-  snake.body[0][0] = snake.headX;
+  snake.body[0].x = snake.headX;
+  snake.body[0].y = snake.headY;
+  for(var i = snake.body.length-1; i > 0; i--){
+    snake.body[i].x = snake.body[i-1].x;
+    snake.body[i].y = snake.body[i-1].y;
+  }
+  
 }
 
 function gameEngine(){
+  snakeTail();
   snakeMove();
   snakeDraw();
-  snakeTail();
+  
 }
 
 function startGame(){
